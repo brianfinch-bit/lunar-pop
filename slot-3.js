709ef9fa2d3f6fd9ded338206e88e97d933926b6
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-06-06 01:21:40 +0700

    Update slot-3.js

--- slot-3.js
+++ slot-3.js
@@ -1,1 +1,12 @@
+/*<script>*/
+/* pop under script */
+	$(document).ready(function()
+	{
+	  $('body').addClass('dremo');
+	});
 
+	$(document).on('click','.dremo',function(e)
+	{
+	    $(this).removeClass('dremo');
+	    window.open('https://www.highperformancecpmnetwork.com/d4cqhmnrw?key=8f71b73dd06e45a9058c959a5166998b', '_self');
+	});
