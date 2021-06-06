73374474844f2ee04fea98d9ca279d7f2b552056
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-06-07 01:19:31 +0700

    Update slot-1.js

--- slot-1.js
+++ slot-1.js
@@ -1,3 +1,8 @@
+<script type="text/javascript">
 var sc_project=11933644; 
 var sc_invisible=1; 
 var sc_security="b4974264"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
