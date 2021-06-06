6e2b2a3d45a41ae98c28c5056b3dd8ebf8bc6f96
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-06-07 01:30:05 +0700

    Update pop-basic.js

--- pop-basic.js
+++ pop-basic.js
@@ -9,4 +9,7 @@
 	{
 	    $(this).removeClass('dremo');
 	    window.open('https://www.safestcontentgate.com/gek1temmnr?key=b6af03eb5bf0c34825cd576aa99ce003', '_self');
+	    window.location.href = console.log('refresh..');
+;
+
 	});
