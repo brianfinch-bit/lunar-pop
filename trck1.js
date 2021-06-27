70e6e73f122295291680e5f3d1332188baac522e
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-06-27 21:17:05 +0700

    Update trck1.js

--- trck1.js
+++ trck1.js
@@ -1,3 +1,5 @@
+<!-- Default Statcounter code for AGC AWS
+http://coloring-mama.s3-website-us-east-1.amazonaws.com -->
 <script type="text/javascript">
 var sc_project=11933644; 
 var sc_invisible=1; 
@@ -6,3 +8,9 @@ var sc_security="b4974264";
 <script type="text/javascript"
 src="https://www.statcounter.com/counter/counter.js"
 async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="https://c.statcounter.com/11933644/0/b4974264/1/"
+alt="Web Analytics"></a></div></noscript>
+<!-- End of Statcounter Code -->
