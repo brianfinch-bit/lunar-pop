c7c38155e559f5511ada431115e8d144fcf8f877
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-07-23 23:53:26 +0700

    Update slot-2.js

--- slot-2.js
+++ slot-2.js
@@ -1,3 +1,2 @@
-var ars = 'https://mrharga.com';
-
-if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1662098130761985"
+     crossorigin="anonymous"></script>
